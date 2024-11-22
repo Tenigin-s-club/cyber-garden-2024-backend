95ea3039177323f8b509346fe4cb804cce54985f
lifelongCat <lifelongcat@yandex.ru>
2024-11-23 01:44:21 +0300

    create base repository

--- app/repositories/base.py
+++ app/repositories/base.py
@@ -1,4 +1,9 @@
 from abc import ABC, abstractmethod
+from sqlalchemy import select, insert, update, delete
+from typing import Union
+from uuid import UUID
+
+from app.db.base import async_session_maker
 
 
 class AbstractRepository(ABC):
@@ -26,3 +31,47 @@ class AbstractRepository(ABC):
     @abstractmethod
     async def delete(**filter_by):
         pass
+
+
+class BaseRepository(AbstractRepository):
+    model = None
+    model_pydantic_schema = None
+
+    @classmethod
+    async def find_one_or_none(cls, **filter_by) -> Union[model_pydantic_schema, None]:
+        async with async_session_maker() as session:
+            query = select(cls.model.__table__.columns).filter_by(**filter_by)
+            result = await session.execute(query)
+            mapping_result = result.mappings().one_or_none()
+            return cls.model_pydantic_schema(**mapping_result) if mapping_result else None
+
+    @classmethod
+    async def find_all(cls, **filter_by) -> list[model_pydantic_schema]:
+        async with async_session_maker() as session:
+            query = select(cls.model.__table__.columns).filter_by(**filter_by)
+            result = await session.execute(query)
+            mapping_result = result.mappings().all()
+            return [cls.model_pydantic_schema(**elem) for elem in mapping_result] if mapping_result else None
+
+    @classmethod
+    async def create(cls, **values) -> UUID:
+        async with async_session_maker() as session:
+            query = insert(cls.model).values(**values).returning(cls.model.id)
+            result = await session.execute(query)
+            await session.commit()
+            return result.scalar()
+
+    @classmethod
+    async def update(cls, id_, **values) -> UUID:
+        async with async_session_maker() as session:
+            query = update(cls.model).filter_by(id=id_).values(**values).returning(cls.model.id)
+            result = await session.execute(query)
+            await session.commit()
+            return result.scalar()
+
+    @classmethod
+    async def delete(cls, **filter_by) -> None:
+        async with async_session_maker() as session:
+            query = delete(cls.model).filter_by(**filter_by)
+            await session.execute(query)
+            await session.commit()
