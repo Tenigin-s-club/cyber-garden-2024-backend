bfd27b9da6db3a7ed561df5575c6c5f3f339de47
lifelongCat <lifelongcat@yandex.ru>
2024-11-22 23:56:03 +0300

    fix migrations (thx ilya) x4

--- app/schemas/users.py
+++ app/schemas/users.py
@@ -8,6 +8,13 @@ class SLoginUser(BaseModel):
     password: str
 
 
+class SInfoUser(BaseModel):
+    fio: str
+    email: EmailStr
+    position: str
+    role_id: int
+
+
 class SRegisterUser(BaseModel):
     fio: str
     email: EmailStr
