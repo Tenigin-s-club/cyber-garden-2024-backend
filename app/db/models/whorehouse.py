ab91e9d2b4e169ab4f1345395b2840aa0b9c4eba
k <iliakripa@mail.ru>
2024-11-23 21:21:54 +0300

    fix migrations

--- app/db/models/whorehouse.py
+++ app/db/models/whorehouse.py
@@ -8,7 +8,7 @@ class WhoreHouse(Base):
     __tablename__ = "whorehouse"
     
     id: Mapped[int] = mapped_column(primary_key=True)
-    office_id: Mapped[int] = mapped_column(ForeignKey("offices.id"))
-    inventory_id: Mapped[int] = mapped_column(ForeignKey("inventory.id"), unique=True)
+    office_id: Mapped[int] = mapped_column(ForeignKey("offices.id", ondelete="CASCADE"))
+    inventory_id: Mapped[int] = mapped_column(ForeignKey("inventory.id", ondelete="CASCADE"), unique=True)
     
     
